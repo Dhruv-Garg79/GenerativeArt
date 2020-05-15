f26381eab532b844b03a2dec7aef5d71622e53a7
Dhruv Garg <garg.dh123@gmail.com>
2020-05-15 18:56:36 +0530

    animation added

--- app/src/main/java/com/example/generativeart/ArtCanvas.kt
+++ app/src/main/java/com/example/generativeart/ArtCanvas.kt
@@ -4,41 +4,47 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.view.View
+import kotlinx.coroutines.*
 import kotlin.math.*
 
 class ArtCanvas(context: Context) : View(context) {
 
     var w = 0
     var h = 0
+    var iter = 6
+    var initIter = 6
 
     private val paint = Paint().apply {
         strokeWidth = 8f
-        isAntiAlias = true // smooths out edges of what is drawn without affecting shape
-        isDither =
-            true // Dithering affects how colors with higher-precision than the device are down-sampled.
-        style = Paint.Style.STROKE // default: FILL
-        strokeJoin = Paint.Join.ROUND // default: MITER
-        strokeCap = Paint.Cap.ROUND // default: BUTT
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.let { renderPainting(it, 6, 6) }
+        canvas?.let { renderAnim(it) }
     }
 
-    private fun renderPainting(canvas: Canvas, iter: Int, initIter: Int) {
-        if (iter == 0)
-            return
-
-        var distance = 0.0
+    private fun renderAnim(canvas: Canvas) = runBlocking {
         val c = (Math.random() * 16777215).toInt() or (0xFF shl 24)
         paint.apply {
             color = c
         }
+
+        CoroutineScope(Dispatchers.Default).launch {
+            renderPainting(canvas)
+        }
+    }
+
+    private suspend fun renderPainting(canvas: Canvas){
+        var distance = 0.0
         for (i in 0..1000) {
             canvas.drawCircle(
-                (w/2 +(distance * atan(distance) * sin(distance) * cos(distance))).toFloat(),
-                ((distance * tan(distance) * sin(distance) * cos(distance))).toFloat(),
+                (w / 2 + (distance * atan(distance) * sin(distance) * cos(distance))).toFloat(),
+                (20 + (distance * tan(distance) * sin(distance) * cos(distance))).toFloat(),
                 6f - (0.5f * (initIter - iter)),
                 paint
             )
@@ -46,7 +52,16 @@ class ArtCanvas(context: Context) : View(context) {
             distance += 0.2 + (0.2 * (initIter - iter));
         }
 
-        renderPainting(canvas, iter-1, initIter)
+        delay(200)
+
+        if (iter > 0) {
+            iter--
+            invalidate()
+        }
+        else{
+            iter = initIter
+            invalidate()
+        }
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
